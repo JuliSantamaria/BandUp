f1eaffb822faaa3fe415c39b63372588aabcdfba
Julian <Julisantamaria@uade.edu.ar>
2024-05-17 21:29:21 -0300

    Agrego el proyecto desde 0

---
+++ App.js
@@ -0,0 +1,10 @@
+import {StyleSheet, View} 
+from 'react-native'
+import Main from "./src/components/Main.jsx"
+
+export default function App() {
+  return (
+      <Main />
+  )
+}
+
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import react from "react"
+import Constants from "expo-constants"
+import { Text, View } from "react-native"
+
+
+const Main = () => {
+    return (
+        <View style={StyleSheet.container}>
+            <Text>Hello world</Text>
+        </View>
+    )
+}
+
+export default Main
